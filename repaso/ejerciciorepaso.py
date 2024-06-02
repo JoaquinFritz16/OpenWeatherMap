f2590ffcf8aca37a29d446ca93f475e3d036d550
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2024-06-02 14:34:02 -0300

    Os

--- repaso/ejerciciorepaso.py
+++ repaso/ejerciciorepaso.py
@@ -2,13 +2,18 @@ import requests
 import pyperclip
 import json
 import webbrowser
-
+import os
 url='https://jsonplaceholder.typicode.com/posts/1'
 response=requests.get(url)
 data=response.json()
 titulo=data['title']
 contenido=data['body']
-filename='post.txt'
+directorio_post='post'
+filename=os.path.join(directorio_post, 'post.txt')
+if not os.path.exists(directorio_post):
+    os.makedirs(directorio_post)
+if os.path.exists(filename):
+    os.remove(filename)
 with open(filename, 'w') as file:
     file.write(f"Titulo: {titulo}\n\n Contenido:{contenido}")
     print('Datos guardados en post.txt')
