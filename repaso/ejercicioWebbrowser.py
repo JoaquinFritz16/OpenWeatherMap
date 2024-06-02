320275f175e99a13ee3d5c715f97b530203d31b0
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2024-06-02 14:26:07 -0300

    Webbrowser

---
+++ repaso/ejercicioWebbrowser.py
@@ -0,0 +1,3 @@
+import webbrowser
+url='https://github.com/JoaquinFritz16/OpenWeatherMap/blob/master/practico2.py'
+webbrowser.open(url)
--- repaso/ejerciciorepaso.py
+++ repaso/ejerciciorepaso.py
@@ -1,17 +1,19 @@
 import requests
 import pyperclip
 import json
+import webbrowser
 
 url='https://jsonplaceholder.typicode.com/posts/1'
 response=requests.get(url)
 data=response.json()
 titulo=data['title']
 contenido=data['body']
-with open('post.txt', 'w') as file:
+filename='post.txt'
+with open(filename, 'w') as file:
     file.write(f"Titulo: {titulo}\n\n Contenido:{contenido}")
     print('Datos guardados en post.txt')
-with open('post.txt', 'r') as file:
+with open(filename, 'r') as file:
     texto=file.read()
     pyperclip.copy(texto)
-
-print("Contenido copiado en el portapapeles")
+webbrowser.open(f"file:///{filename}")
+print("Contenido copiado en el portapapeles y archivo abierto en el navegador")
